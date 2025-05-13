3dcb722984642ef05063a73d66c74a22590d7262
NET14_NgocQuan <quantnse170175@fpt.edu.vn>
2025-05-13 12:26:13 +0700

    Add assets

---
+++ Ouroboros_Elio/Controllers/ProductController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Ouroboros_Elio.Controllers
+{
+    public class ProductController : Controller
+    {
+        public IActionResult ProductList()
+        {
+            return View();
+        }
+    }
+}
