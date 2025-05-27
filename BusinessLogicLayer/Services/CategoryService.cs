d254cbfe4a8d26bd6ac2207eea94f7d71f021d24
NghiaHT <huynhtrongnghia15n@gmail.com>
2025-05-27 14:04:53 +0700

    Cập nhật các method để làm trang Home

---
+++ BusinessLogicLayer/Services/CategoryService.cs
@@ -0,0 +1,28 @@
+﻿using AutoMapper;
+using BusinessLogicLayer.Dtos.CategoryDtos;
+using BusinessLogicLayer.ServiceContracts;
+using DataAccessLayer.RepositoryContracts;
+
+namespace BusinessLogicLayer.Services;
+
+public class CategoryService : ICategoryService
+{
+    private readonly ICategoryRepository _categoryRepository;
+    private readonly IMapper _mapper;
+
+    public CategoryService(ICategoryRepository categoryRepository, IMapper mapper)
+    {
+        _categoryRepository = categoryRepository;
+        _mapper = mapper;
+    }
+
+    public async Task<List<CategoryViewModel>?> GetAllCategoriesAsync()
+    {
+        var categories = await _categoryRepository.GetAllCategoriesAsync();
+        if (categories == null)
+        {
+            return null;
+        }
+        return _mapper.Map<List<CategoryViewModel>>(categories);
+    }
+}
